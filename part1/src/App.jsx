8331ab23fac2412004da66460ad53f3aabde25ab
olivia mccaster <your.mccastero@gmail.com>
2024-08-14 09:18:22 -0500

    new changes to app file

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -6,29 +6,34 @@ import Total from './Total';
 
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-  {
-    name:  'Fundamentals of React',
-   exercise: 10
-  },
-  {
-    name:'Using props to pass data',
-    exercise: 7
-  },
-  {
-    name:'State of a component',
-   exercise: 14 
-
-  function Total(prop.Total) 
-    return (
-       <> 
-       <><Header course={course} /><Content(prop.Content)/><Total /></>
-       </> 
-
+  const course =  'Half Stack application development'
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
+  return ( 
+    
+    <> 
+      <Header course={course} />
+      <Content/>
+      <Total />
+    </>
     
   )
+}
+
+ 
+   
   
 
 export default App
